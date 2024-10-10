38ababf005323231b739ad2a383d93f01e8e0ee3
Katt4722 <164028253+Katt4722@users.noreply.github.com>
2024-10-10 11:59:40 -0300

    cambio

--- Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc1340340c227f7f7d943a087c611422ec19d2b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e500d0df4397aa505da7f44d8df63632c58e8dbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
