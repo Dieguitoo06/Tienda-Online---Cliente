e500d0df4397aa505da7f44d8df63632c58e8dbf
Dieguitoo06r <ds181689@gmail.com>
2024-10-09 22:58:57 -0300

    si

--- Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35163b748d91f76e6aa6ffbbe94d35c625a73310")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc1340340c227f7f7d943a087c611422ec19d2b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
